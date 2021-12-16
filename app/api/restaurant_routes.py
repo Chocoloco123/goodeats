b09241914d0b77bd2d33598b223454528487492a
Chocoloco123 <csarkki@gmail.com>
2021-12-16 09:30:01 -0800

    removed restaurant routes file from this branch

--- app/api/restaurant_routes.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint, jsonify, request
-from app.models import Product, db, Cart
-from app.forms import NewProductForm
-from app.forms import EditProductForm
-
-from flask_login import current_user
