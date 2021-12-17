76858d403a87578f57a54b3d37aaad14fba6835d
Chocoloco123 <csarkki@gmail.com>
2021-12-17 10:01:40 -0800

    added singleRestaurant page elements and started styling

--- react-app/src/components/SingleRestaurant/index.js
+++ react-app/src/components/SingleRestaurant/index.js
@@ -2,6 +2,7 @@ import { useParams, NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getOneRestaurant } from "../../store/restaurants";
+import './SingleRestaurant.css'
 
 const SingleRestaurantPage = () => {
   const { id } = useParams()
@@ -23,11 +24,49 @@ const SingleRestaurantPage = () => {
   } else {
     return (
       <div>
-        hello world!
-        {restaurant.name}
-        <img src={restaurant.imageUrl} alt=''></img>
+        <div>
+          <div className='restaurantIntroNavDiv'>
+            <div className="restaurantIntroInnerDiv">
+              {restaurant.name}
+            </div>
+            <div className="starsAndReviewsDiv">
+              <div className="starsDiv">
+                Stars: {restaurant.stars}
+              </div>
+              <div>
+                review count: {restaurant.review_count}
+              </div>
+            </div>
+            <div className="singleRestaurantCategoryDiv">
+              The category: {restaurant.categoryId}
+            </div>
+          </div>
+        </div>
+        <div className='reviewAndAddPhotoDiv'>
+          <button className='writeAReviewBtn'><i className="far fa-star"></i> Write a Review</button>
+          {/* <button>Add Photo</button> */}
+          <div className="locationAndHoursDiv">
+            <h3>Location & Hours</h3>
+            {restaurant.hours}
+          </div>
+          <div>
+            <img src={restaurant.imageUrl} alt='' className="singleImage"></img>
+          </div>
+        </div>
+        <div className="aboutAndContactDiv">
+          <div className="restaurantAboutDiv">
+            <h3>
+              About the Business
+            </h3>
+            {restaurant.description}
+          </div>
+          <div className="restaurantContactDiv">
+            {restaurant.websiteUrl}
+            {restaurant.phoneNumber}
+          </div>
+        </div>
       </div>
-
+      // import reviews component here
 
     )
 
