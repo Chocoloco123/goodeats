3eac92d746c98b4fc098001b633df14a3b71d3d2
Chocoloco123 <csarkki@gmail.com>
2021-12-17 16:24:07 -0800

    updates to add restaurant form and updates to POST route

--- app/api/restaurants_routes.py
+++ app/api/restaurants_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from app.models import Restaurant, db
+from app.forms import NewRestaurantForm
 
 from flask_login import current_user
 
@@ -31,5 +32,22 @@ def one_restaurant(id):
 # add new restaurant
 @restaurant_routes.route('/new_restaurant', methods=["POST"])
 def add_restaurant():
-  new_restaurant = Restaurant
+  new_restaurant_form = NewRestaurantForm()
+  new_restaurant_form['csrf_token'].data = request.cookies['csrf_token']
+  if new_restaurant_form.validate_on_submit():
+    restaurant = Restaurant(
+      name = new_restaurant_form.data['name'],
+      description = new_restaurant_form.data['description'],
+      address = new_restaurant_form.data['address'],
+      city = new_restaurant_form.data['city'],
+      state = new_restaurant_form.data['state'],
+      zipcode = new_restaurant_form.data['zipcode'],
+      hours = new_restaurant_form.data['hours'],
+      priceRating = new_restaurant_form.data['priceRating'],
+      phoneNumber = new_restaurant_form.data['phoneNumber'],
+      websiteUrl = new_restaurant_form.data['websiteUrl'],
+      imageUrl=new_restaurant_form.data['websiteUrl'],
+      category_id=int(new_restaurant_form.data['category']),
+      ownerId=current_user['id']
+    )
 
