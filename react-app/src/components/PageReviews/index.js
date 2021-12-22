a2bd37e3b41d2424e858e0bd19fc5e584c4a100a
Chocoloco123 <csarkki@gmail.com>
2021-12-21 23:38:46 -0800

    progress on rendering the reviews. still need to fix rendering bug that compounds reviews

--- react-app/src/components/PageReviews/index.js
+++ react-app/src/components/PageReviews/index.js
@@ -11,21 +11,42 @@ const GetAllReviews = ({restaurant}) => {
   // console.log('theReviews:', reviews)
 
   const sessionUser = useSelector((state) => state.session.user);
+  const [users, setUsers] = useState([])
 
   const { id } = useParams()
+
+  useEffect(() => {
+    async function getUsers (){
+      const res = await fetch('/api/users/');
+      const resUsers = await res.json();
+      setUsers(resUsers.users)
+    }
+    getUsers();
+  }, [])
   
   useEffect(() => {
     dispatch(getPageReviews(id))
   // }, [dispatch, reviews.length, id])
   }, [dispatch, id])
-  
+  // ! this might need revision
+  const reviewUsers = (userId) => {
+    const showUsername = users?.filter((user) => {
+      return user.id === userId
+    });
+
+    if (showUsername) {
+      return showUsername[0]?.username
+    } else {
+      return null
+    }
+  }
   
 
   return (
     <div>
       {reviews.map((review) => 
       <div>
-        {sessionUser.username}
+        {reviewUsers(review.userId)}
         {review.created_at}
         {Array(review.rating).fill(
           <span><i className="fas fa-star fa-xs"></i></span>).map((el, idx) => 
