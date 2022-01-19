7f2be93b6c7f7170389af58626df10aa714e0d4d
Chocoloco123 <csarkki@gmail.com>
2022-01-19 09:32:48 -0800

    added handling for submit button and enter for search

--- react-app/src/components/Search/SearchRestaurants/index.js
+++ react-app/src/components/Search/SearchRestaurants/index.js
@@ -2,20 +2,31 @@ import { React, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { searchForRestaurant } from '../../../store/search';
 import { useHistory } from 'react-router';
-import goodeatsFlower from '../../../media/goodeats_flower_transparent.png'
+// import goodeatsFlower from '../../../media/goodeats_flower_transparent.png'
 
 const SearchBar = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const [search, setSearch] = useState('');
 
-  const handleSearch = async(e) => {
+  const handleEnter = async(e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      let data = await dispatch(searchForRestaurant(search));
+  
+      if (data) {
+        history.push(`/search/${search}`)
+      }
+    }
+  }
+
+  const handleSearchClick = async(e) => {
     e.preventDefault();
     let data = await dispatch(searchForRestaurant(search));
-
-    if (data) {
-      history.push(`/search/${search}`)
-    }
+  
+      if (data) {
+        history.push(`/search/${search}`)
+      }
   }
 
   return (
@@ -30,12 +41,12 @@ const SearchBar = () => {
         placeholder='Find the Best Restaurants in Town'
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        onKeyPress={(e) => handleSearch(e)}
+        onKeyDown={(e) => handleEnter(e)}
         className='centerText-Main-Div2'
         >
       </input>
       <div>
-        <button type="submit"><i className="fas fa-search"></i></button>
+        <button type="submit" onClick={(e) => handleSearchClick(e)}><i className="fas fa-search"></i></button>
       </div>
       </div>
     </form>
